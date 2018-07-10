8e81534082e850d27321c47f489f5abb89a277c3
Suneel <suneellamani77@gmail.com>
2018-07-10 11:49:26 +0530

    modified test1.java and added index.html

--- test1.java
+++ test1.java
@@ -1,10 +1,8 @@
- Volume in drive E is New Volume
- Volume Serial Number is A8B1-B552
+public class test1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("hello words...");
+	}
 
- Directory of E:\SJ\GIT\My Project
-
-07/10/2018  11:12 AM    <DIR>          .
-07/10/2018  11:12 AM    <DIR>          ..
-07/10/2018  11:12 AM                 0 test1.java
-               1 File(s)              0 bytes
-               2 Dir(s)  380,949,475,328 bytes free
+}
